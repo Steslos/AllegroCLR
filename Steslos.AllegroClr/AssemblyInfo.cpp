d32aa6343cb5d0f31fd2d97c051aff7d69385728
Steslos <rinon32@yahoo.com>
2021-01-01 16:34:49 -0800

    adding dependencies

---
+++ Steslos.AllegroClr/AssemblyInfo.cpp
@@ -0,0 +1,20 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"AllegroCLR")];
+[assembly:AssemblyDescriptionAttribute(L"DotNET Core wrapper library for using the Allegro game programming library from C#.")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"")];
+[assembly:AssemblyProductAttribute(L"Steslos.AllegroClr")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) 2020")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute("1.0.*")];
+
+[assembly:ComVisible(false)];
+
+[assembly:CLSCompliantAttribute(true)];
