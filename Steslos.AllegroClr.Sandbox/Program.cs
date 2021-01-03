647a2f43800372f627f935d258acee8adeecb62a
Steslos <rinon32@yahoo.com>
2021-01-02 16:50:17 -0800

    adjusted build locations. added sandbox project

---
+++ Steslos.AllegroClr.Sandbox/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Steslos.AllegroClr.Sandbox
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Al.InstallSystem();
+            var display = Al.CreateDisplay(1280, 720);
+
+            Al.ClearToColor(Al.MapRgb(64, 128, 255));
+            Al.FlipDisplay();
+            Al.Rest(3);
+
+            Al.DestroyDisplay(display);
+            Al.UninstallSystem();
+        }
+    }
+}
